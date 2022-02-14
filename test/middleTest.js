b3a1354f49fc7cfc7562977d133dd6545e2d00c6
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-14 23:31:16 +0000

    lastest updated version

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
+
+describe("#middle", () => {
+  it("should return an empty array if array value is < 3", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("should return the middle number if array lenght is odd", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return the middle two integers if the array lenght is even", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5])
+  })
+});
