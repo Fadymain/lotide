4657b138801d476357e7f1342e31ce2a1ba1dc41
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-14 23:19:49 +0000

    updated tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,14 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+describe("tail", () => {
+  it("returns [Lighthouse, Labs] for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  })
+})
 
-console.log(tail(words));
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+// console.log(tail(words));
+// assertEqual(words.length, 3);
