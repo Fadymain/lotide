3faa5f96065151c0ddf51d35e9ce0dd258bddab3
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 02:42:00 +0000

    countOnlyTest completed

---
+++ test/countOnlyTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly');
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+describe("#countOnly", () => {
+  it("sould return how many times a name is present in the object", () => {
+    assert.deepEqual(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }), { Fang: 2, Jason: 1 });
+  })
+});
