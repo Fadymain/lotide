3b88059de520eabd2647533d0ed774a6f4d2d710
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 01:38:24 +0000

    takeUntilTest completed

---
+++ test/takeUntilTest.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+const takeUntil = require('../takeUntil');
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+
+describe("#takeUntil", () => {
+  it("should return [1, 2, 5, 7, 2] when the array is [1, 2, 5, 7, 2, -1, 2, 4, 5]", () => {
+    assert.deepEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+  });
+
+  it("should return [I've, been, to, Hollywood] when the array is [I've, been, to, Hollywood, ,, I've, been, to, Redwood]", () => {
+    assert.deepEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+  })
+});
+
+
+
+
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+// console.log('---');
+
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
