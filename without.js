519a08b0660112a8cce3969fc40a9e31e81795f8
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 00:50:02 +0000

    updated version

--- without.js
+++ without.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  } 
-  return true;
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`${arr1} = ${arr2}`);
-  } else {
-    console.log(`${arr1} != ${arr2}`);
-  }
-}
-
 const without = function (arr1, arr2) {
   let newArr = [];
   for (let i = 0; i < arr1.length; i++) {
@@ -27,11 +7,5 @@ const without = function (arr1, arr2) {
   }
   return newArr;
 }
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-// assertArraysEqual(eqArrays(without(words, ["hello", "world", "lighthouse"])));
-assertArraysEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]), true);
+module.exports = without;
