3a57f3ac32c30d5b93daa96cefa62f7e48d27150
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 03:47:46 +0000

    updated version

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,15 +1,4 @@
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } 
-  for (let key in object1) {
-      if (typeof object1[key] === "string" && object1[key] !== object2[key]) {
-        return false;
-      } else if (JSON.stringify(object1[key]) !== JSON.stringify(object2[key])) {
-        return false;
-      }
-  } return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
@@ -19,17 +8,5 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertObjectsEqual(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertObjectsEqual(ab, abc)); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertObjectsEqual(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertObjectsEqual(cd, cd2)); // => false
+module.exports = assertObjectsEqual;
 
