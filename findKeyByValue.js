65a88154a2eed7ff4345089ef319de5e2df9b3a4
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 00:36:14 +0000

    updated version of findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(object, value) {
   for (let obj in object) {
-    console.log(obj);
     if (object[obj] === value) {
       return obj;
     }
@@ -16,11 +7,6 @@ const findKeyByValue = function(object, value) {
 }
 
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = findKeyByValue;
