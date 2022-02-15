b5e9c5d5ed798e294c3c42bd2a0e7c1c4e6c5e0d
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 03:50:55 +0000

    assertObjectEqualTest completed

---
+++ test/assertObjectEqualTest.js
@@ -0,0 +1,13 @@
+const assertObjectEqual = require('../assertObjectEqual');
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+assertObjectEqual(ab, ba);
+assertObjectEqual(ab, abc);
+assertObjectEqual(cd, dc);
+assertObjectEqual(cd, cd2);
