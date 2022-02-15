c07d38c1d0300721e94f2039b2f329382455a57f
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 00:37:16 +0000

    findKeyByValueTest completed

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,21 @@
+const findKeyByValue = require('../findKeyByValue');
+const assert = require('chai').assert;
+
+describe("#findKeyByValue", () => {
+  it("should return drama when the value is The wire", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+
+  it("should return undefined when the value is not in the object", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined)
+  })
+})
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
