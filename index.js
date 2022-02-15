d25ff445a42131b45fb6d554915644b952ce0ecf
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-15 03:58:11 +0000

    updated version

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const findKeyByValue = require('./findKeyByValue');
+const without = require('./without');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const letterPositions = require('./letterPositions');
+const findKey = require('./findkey');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectEqual = require('./assertObjectEqual');
 
 
 module.exports = {
@@ -12,5 +22,15 @@ module.exports = {
   middle,
   eqArrays,
   assertEqual,
-  assertArraysEqual
+  assertArraysEqual,
+  findKeyByValue,
+  without,
+  map,
+  takeUntil,
+  letterPositions,
+  findKey,
+  eqObjects,
+  countOnly,
+  countLetters,
+  assertObjectEqual
 };
